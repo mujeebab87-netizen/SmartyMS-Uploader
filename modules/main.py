45ea15f457b4293722940695e532e4e90ede73f6
mujeebab87-netizen <mujeebab87@gmail.com>
2026-04-19 20:50:46 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -311,8 +311,8 @@ async def txt_handler(bot: Client, m: Message):
 
             try:  
                 
-                cc = f'**📹 VID_ID: {str(count).zfill(3)}.\n\n📝 Title: {name1} {res}.mkv\n\n<pre><code>📚 Batch Name: {b_name}</code></pre>\n\n📥 Extracted By♠ : {CR}\n\n**∘₊✦━━━✦❖𓆩Mʀ Tᴏxɪᴄ𓆪🦅❖━━━✦₊∘**'
-                cc1 = f'**💾 PDF_ID: {str(count).zfill(3)}.\n\n📝 Title: {name1} .pdf\n\n<pre><code>📚 Batch Name: {b_name}</code></pre>\n\n📥 Extracted By♠ : {CR}\n\n**∘₊✦━━━✦❖𓆩Mʀ Tᴏxɪᴄ𓆪🦅❖━━━✦₊∘**'
+                cc = f'**📹 VID_ID: {str(count).zfill(3)}.\n\n📝 Title: {name1} {res}.mkv\n\n<pre><code>📚 Batch Name: {b_name}</code></pre>\n\n📥 Extracted By♠ : {CR}\n\n**∘₊❀╬━━⫷⫸𝐀𝐋𝐈⫷⫸━━╬❀₊∘**'
+                cc1 = f'**💾 PDF_ID: {str(count).zfill(3)}.\n\n📝 Title: {name1} .pdf\n\n<pre><code>📚 Batch Name: {b_name}</code></pre>\n\n📥 Extracted By♠ : {CR}\n\n**∘₊❀╬━━⫷⫸𝐀𝐋𝐈⫷⫸━━╬❀₊∘**'
                     
                 
                 if "drive" in url:
